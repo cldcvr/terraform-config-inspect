cb57c7d113d62a5b0526c9e629fe4939bd8141c5
Kanak Singhal <kanak@cldcvr.com>
2023-08-01 19:30:14 +0530

    TER-151 add block end line
    add the end line number as well to the block position struct
    along with the existing start line number

--- tfconfig/source_pos.go
+++ tfconfig/source_pos.go
@@ -6,6 +6,7 @@ package tfconfig
 import (
 	legacyhcltoken "github.com/hashicorp/hcl/hcl/token"
 	"github.com/hashicorp/hcl/v2"
+	"github.com/hashicorp/hcl/v2/hclsyntax"
 )
 
 // SourcePos is a pointer to a particular location in a source file.
@@ -15,8 +16,11 @@ import (
 // is usually the first line of its definition, although the definition can
 // be a little "fuzzy" with JSON-based config files.
 type SourcePos struct {
-	Filename string `json:"filename"`
-	Line     int    `json:"line"`
+	Filename  string `json:"filename"`
+	Line      int    `json:"line"`
+	EndLine   int    `json:"end_line"`
+	StartByte int
+	EndByte   int
 }
 
 func sourcePos(filename string, line int) SourcePos {
@@ -26,14 +30,35 @@ func sourcePos(filename string, line int) SourcePos {
 	}
 }
 
+func sourceBlockHCL(block *hcl.Block) SourcePos {
+	pos := SourcePos{
+		Filename:  block.DefRange.Filename,
+		Line:      block.DefRange.Start.Line,
+		StartByte: block.DefRange.Start.Byte,
+		EndLine:   block.DefRange.End.Line,
+		EndByte:   block.DefRange.End.Byte,
+	}
+
+	body := block.Body.(*hclsyntax.Body)
+	if body != nil {
+		pos.EndByte = body.SrcRange.End.Byte
+		pos.EndLine = body.SrcRange.End.Line
+	}
+
+	return pos
+}
+
 func sourcePosHCL(rng hcl.Range) SourcePos {
 	// We intentionally throw away the column information here because
 	// current and legacy HCL both disagree on the definition of a column
 	// and so a line-only reference is the best granularity we can do
 	// such that the result is consistent between both parsers.
 	return SourcePos{
-		Filename: rng.Filename,
-		Line:     rng.Start.Line,
+		Filename:  rng.Filename,
+		Line:      rng.Start.Line,
+		EndLine:   rng.End.Line,
+		StartByte: rng.Start.Byte,
+		EndByte:   rng.End.Byte,
 	}
 }
 
